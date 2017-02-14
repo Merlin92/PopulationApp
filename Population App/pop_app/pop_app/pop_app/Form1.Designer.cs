62acda8bbffd044c8d84150399125efdad9e3502
Merlin Bierekofen <merlin.bierekofen@gmail.com>
2017-02-14 17:11:15 +0000

    1. Commit
    Set-up code for Population App program on git-hub

---
+++ Population App/pop_app/pop_app/pop_app/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace pop_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.annualTotalChangeListBox = new System.Windows.Forms.ListBox();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.programmerLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.avAnChangelabel = new System.Windows.Forms.Label();
+            this.greatestIncreaseLabel = new System.Windows.Forms.Label();
+            this.leastIncreaseLabel = new System.Windows.Forms.Label();
+            this.avAnChangeOutputLabel = new System.Windows.Forms.Label();
+            this.greatestIncreaseOutputLlabel = new System.Windows.Forms.Label();
+            this.lestIncreaseOutputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // annualTotalChangeListBox
+            // 
+            this.annualTotalChangeListBox.FormattingEnabled = true;
+            this.annualTotalChangeListBox.Location = new System.Drawing.Point(15, 30);
+            this.annualTotalChangeListBox.Name = "annualTotalChangeListBox";
+            this.annualTotalChangeListBox.Size = new System.Drawing.Size(247, 199);
+            this.annualTotalChangeListBox.TabIndex = 0;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(329, 188);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 1;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // programmerLabel
+            // 
+            this.programmerLabel.AutoSize = true;
+            this.programmerLabel.Location = new System.Drawing.Point(447, 240);
+            this.programmerLabel.Name = "programmerLabel";
+            this.programmerLabel.Size = new System.Drawing.Size(165, 13);
+            this.programmerLabel.TabIndex = 2;
+            this.programmerLabel.Text = "Programmed by Merlin Bierekofen";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Increase in Irish population from 1950 to 1990";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(410, 188);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(491, 188);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 5;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // avAnChangelabel
+            // 
+            this.avAnChangelabel.AutoSize = true;
+            this.avAnChangelabel.Location = new System.Drawing.Point(300, 42);
+            this.avAnChangelabel.Name = "avAnChangelabel";
+            this.avAnChangelabel.Size = new System.Drawing.Size(184, 13);
+            this.avAnChangelabel.TabIndex = 6;
+            this.avAnChangelabel.Text = "Average annual change in population";
+            // 
+            // greatestIncreaseLabel
+            // 
+            this.greatestIncreaseLabel.AutoSize = true;
+            this.greatestIncreaseLabel.Location = new System.Drawing.Point(300, 91);
+            this.greatestIncreaseLabel.Name = "greatestIncreaseLabel";
+            this.greatestIncreaseLabel.Size = new System.Drawing.Size(198, 13);
+            this.greatestIncreaseLabel.TabIndex = 7;
+            this.greatestIncreaseLabel.Text = "Year with greatest increase in population";
+            // 
+            // leastIncreaseLabel
+            // 
+            this.leastIncreaseLabel.AutoSize = true;
+            this.leastIncreaseLabel.Location = new System.Drawing.Point(300, 141);
+            this.leastIncreaseLabel.Name = "leastIncreaseLabel";
+            this.leastIncreaseLabel.Size = new System.Drawing.Size(182, 13);
+            this.leastIncreaseLabel.TabIndex = 8;
+            this.leastIncreaseLabel.Text = "Year with least increase in population";
+            // 
+            // avAnChangeOutputLabel
+            // 
+            this.avAnChangeOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.avAnChangeOutputLabel.Location = new System.Drawing.Point(505, 37);
+            this.avAnChangeOutputLabel.Name = "avAnChangeOutputLabel";
+            this.avAnChangeOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.avAnChangeOutputLabel.TabIndex = 9;
+            // 
+            // greatestIncreaseOutputLlabel
+            // 
+            this.greatestIncreaseOutputLlabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.greatestIncreaseOutputLlabel.Location = new System.Drawing.Point(505, 87);
+            this.greatestIncreaseOutputLlabel.Name = "greatestIncreaseOutputLlabel";
+            this.greatestIncreaseOutputLlabel.Size = new System.Drawing.Size(100, 23);
+            this.greatestIncreaseOutputLlabel.TabIndex = 10;
+            // 
+            // lestIncreaseOutputLabel
+            // 
+            this.lestIncreaseOutputLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lestIncreaseOutputLabel.Location = new System.Drawing.Point(505, 137);
+            this.lestIncreaseOutputLabel.Name = "lestIncreaseOutputLabel";
+            this.lestIncreaseOutputLabel.Size = new System.Drawing.Size(100, 23);
+            this.lestIncreaseOutputLabel.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 262);
+            this.Controls.Add(this.lestIncreaseOutputLabel);
+            this.Controls.Add(this.greatestIncreaseOutputLlabel);
+            this.Controls.Add(this.avAnChangeOutputLabel);
+            this.Controls.Add(this.leastIncreaseLabel);
+            this.Controls.Add(this.greatestIncreaseLabel);
+            this.Controls.Add(this.avAnChangelabel);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.programmerLabel);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.annualTotalChangeListBox);
+            this.Name = "Form1";
+            this.Text = "Population Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox annualTotalChangeListBox;
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Label programmerLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Label avAnChangelabel;
+        private System.Windows.Forms.Label greatestIncreaseLabel;
+        private System.Windows.Forms.Label leastIncreaseLabel;
+        private System.Windows.Forms.Label avAnChangeOutputLabel;
+        private System.Windows.Forms.Label greatestIncreaseOutputLlabel;
+        private System.Windows.Forms.Label lestIncreaseOutputLabel;
+    }
+}
+
---
+++ Population App/pop_app/pop_app/pop_app/Form1.cs
@@ -0,0 +1,205 @@
+﻿/* Student Name: Merlin Bierekofen
+ * Student ID: 14101805
+ * Date: 13.11.2016
+ * Assignment: 5
+ 
+   In the Assignment 5 folder on Blackboard, you will find a file named “IrishPopulation.txt”. The file
+contains the midyear population of Ireland, during the years 1950 through 1990.
+The first line in the file contains the population for 1950, the second line contains the population for
+1951, and so forth.
+Create an application that reads the file’s contents into an array or a List.
+The application should display the following data in an easy to understand manner
+􀁸 The increase in population each year e.g. (1951 there is a population increase of 31,710)
+􀁸 The average annual change in population during the time period
+􀁸 The year with the greatest increase in population during the time period
+􀁸 The year with the least increase in population during the time period
+1) Handle unexpected errors. (Invalid inputs, file not found etc.)
+2) Follow good programming conventions for object names: include comments for some
+methods and at the top of the file to explain what the application is about.
+3) Give the form a name that will display at the top of the form.
+4) Include your name in the bottom right corner “Programmed by …………….”
+5) Include screenshots showing the application in use.
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pop_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public double Average(int[]iArray)
+        {
+            //variable holding population increase
+            int totIncrease = 0;
+            //variable holding increase from year to year
+            int incPerYear = 0;
+            //variable holding year checked for the for loop
+            int incCount = 0;
+            //variable holding number of real variables in the array
+            int arrayLength = iArray.Length - 1;
+
+            double average;
+
+            for (int index = 0; index < arrayLength; index++)
+            {
+                int index2 = index + 1;
+
+                    int y1Population = iArray[index];
+                    int y2Population = iArray[index2];
+
+                    incPerYear = y2Population - y1Population;
+                    incCount++;
+
+                //adding inc to the var
+                totIncrease = totIncrease + incPerYear;
+            }
+
+            //calc average increase
+            average = totIncrease / incCount;
+            return average;
+        }
+
+        public int Highest(int[] iArray)
+        {
+            //variable holding highest pop increase
+            int highest = 0;
+            //variable holding inc from year to year 
+            int incPerYear = 0;
+            //variable holding number of real variables in the array 
+            int arrayLength = iArray.Length - 1;
+            int firstYear = 1950;
+            int highestYear = 0;
+
+            //loop to calc change in popul.
+            for (int index = 0; index < arrayLength; index++)
+            {
+                int index2 = index + 1;
+                int y1Population = iArray[index];
+                int y2Population = iArray[index2];
+
+                incPerYear = y2Population - y1Population;
+
+                //getting year with the greatest population
+                if (incPerYear > highest)
+                {
+                    highest = incPerYear;
+                    highestYear = firstYear + index2;
+                }
+            }
+
+            return highestYear;
+        }
+
+        public int Lowest(int[] iArray)
+        {
+            //variable holding lowest pop increase
+            int lowest = 1000000;
+            //variable holding inc from year to year 
+            int incPerYear = 0;
+            //variable holding number of real variables in the array 
+            int arrayLength = iArray.Length - 1;
+            int firstYear = 1950;
+            int lowestYear = 0;
+
+            //loop to calc change in popul.
+            for (int index = 0; index < arrayLength; index++)
+            {
+                int index2 = index + 1;
+                int y1Population = iArray[index];
+                int y2Population = iArray[index2];
+
+                incPerYear = y2Population - y1Population;
+
+                //getting year with the lowest population
+                if (incPerYear < lowest)
+                {
+                    lowest = incPerYear;
+                    lowestYear = firstYear + index2;
+                }
+            }
+
+            return lowestYear;
+        }
+        private void calcButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //variable used for the size of the array
+                const int Num = 40;
+
+                //decl array
+                int[] newNum = new int[Num];
+                int index = 0;
+                int hScore;
+                int lScore;
+                int y1 = 1951;
+                int incPerYear = 0;
+                int arrayRealCount = newNum.Length - 1;
+                double avg;
+
+                //use streamreader for input 
+                StreamReader inputFile;
+                inputFile = File.OpenText("IrishPopulation.txt");
+                while (!inputFile.EndOfStream && index < newNum.Length)
+                {
+                    newNum[index] = int.Parse(inputFile.ReadLine());
+                    index++;
+                }
+                inputFile.Close();
+
+                //loop to add years to list box
+                for (int index2 = 0; index2 < arrayRealCount; index2++)
+                {
+                    int index3 = index2 + 1;
+                    int y1Population = newNum[index2];
+                    int y2Population = newNum[index3];
+
+                    incPerYear = y2Population - y1Population;
+                    annualTotalChangeListBox.Items.Add(y1 + " :" + incPerYear);
+                    y1++;
+                }
+
+                //calling meth
+
+                hScore = Highest(newNum);
+                lScore = Lowest(newNum);
+                avg = Average(newNum);
+
+                avAnChangeOutputLabel.Text = avg.ToString("n1");
+                greatestIncreaseOutputLlabel.Text = hScore.ToString("n1");
+                lestIncreaseOutputLabel.Text = lScore.ToString("n1");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            annualTotalChangeListBox.Items.Clear();
+            avAnChangeOutputLabel.Text = "";
+            greatestIncreaseOutputLlabel.Text = "";
+            lestIncreaseOutputLabel.Text = "";
+        }
+    }
+}
